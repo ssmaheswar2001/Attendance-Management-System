4c343d932bf95887ce77be3da52c60fdf99a33fa
ssmaheswar2001 <ssmaheswar2001@gmail.com>
2025-06-29 19:37:46 -0500

    Reverting everything to HTTP.

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(root_path="/api")
+app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
